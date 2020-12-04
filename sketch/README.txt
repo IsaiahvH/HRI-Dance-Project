72ca176e1a8dfa74fe8bfd2cd264ae2d691f31d3
Vriezen, E.C. (Emma) <e.vriezen@student.ru.nl>
2020-12-04 17:32:47 +0100

    Added the WIP_PoseRecorded.py and updated the README accordingly.

--- sketch/README.txt
+++ sketch/README.txt
@@ -6,9 +6,19 @@ MediaPipeTest:
 - Do not use the z-value (depth): model is not trained properly for that.
 - Instructions:
 	- Download the imports (easy through PyCharm)
+	    - If this does not work:
+	    - Open CMD anywhere where you can use the pip command
+	        - (Or in your Venv if you use one.)
+	    - "pip install pipwin"
+	    - "pipwin install mediapipe" (or any other package)
 	- Ensure your camera is connected if it is not built in
 	- Set Python interpreter on one that works (works for Python 3.8 64).
 	- Right mouse click on this file --> Run (in PyCharm)
 	- Run!
 - Close the MediaPipe window by pressing Esc.
 - Tips for classifying poses: https://developers.google.com/ml-kit/vision/pose-detection/classifying-poses
+
+PoseRecorder:
+- STILL WIP ++++ DON'T USE IT YET.
+- Built around the same code as MediaPipeTest.
+- Used for storing poses.
--- sketch/WIP_PoseRecorder.py
+++ sketch/WIP_PoseRecorder.py
@@ -1,16 +1,19 @@
-# Source: https://google.github.io/mediapipe/solutions/pose
 # File by: Emma Vriezen
 # See README for comments.
 
-import cv2
-import mediapipe as mp
+import cv2  # For displaying your webcam stream and the keypoints
+import mediapipe as mp  # For keypoint recognition
+import pandas as pd  # For its dataframes
+
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-# For webcam input:
 pose = mp_pose.Pose(
     min_detection_confidence=0.5, min_tracking_confidence=0.5)
-cap = cv2.VideoCapture(0)  # Change this number to use another webcam!
+cap = cv2.VideoCapture(1)  # Change this number to use another webcam!
+
+key_df = pd.DataFrame()
+
 while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -28,11 +31,15 @@ while cap.isOpened():
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     # print(results.pose_landmarks)  # This is all the landmarks
-    print(results.pose_landmarks.landmark[11])  # This is a specific one
+    print(results.pose_landmarks.landmark[11])
+    print(results.pose_landmarks.landmark[11].x)  # This is a specific one
+
     mp_drawing.draw_landmarks(
         image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
     cv2.imshow('MediaPipe Pose', image)
-    if cv2.waitKey(5) & 0xFF == 27:
+
+    if cv2.waitKey(5) & 0xFF == 27:  # When ESC is pressed, stop the loop:
+        print("Pressed ESC --> end video capture.")
         break
 pose.close()
 cap.release()
